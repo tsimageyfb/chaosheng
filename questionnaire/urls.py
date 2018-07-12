c35beac69a3c6310291c81ae7457f795debf25fa
刘庆良 <liuqingliang@guazi.com>
2018-07-12 16:32:25 +0800

    Add: 实现图片展示

--- questionnaire/urls.py
+++ questionnaire/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.staticfiles import views
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^exam/', include('exam.urls', 'exam')),
-    url(r'^statics/(?P<path>.*)$', include(views.serve)),
+    url(r'^statics/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
 ]
