ce9114a4b867a51e53e6ed68cbdb955e41e9ab14
刘庆良 <liuqingliang@guazi.com>
2018-08-04 22:58:38 +0800

    Add: some fields

--- exam/models.py
+++ exam/models.py
@@ -9,7 +9,7 @@ class Question(models.Model):
     question_type = models.IntegerField(default='1')
     description = models.CharField(max_length=128, default='', blank=True)
     # 选择题的各选项，半角逗号隔开
-    answer_options = models.CharField(max_length=64, default='正常,良性,恶性')
+    answer_options = models.CharField(max_length=64, default='良性,恶性')
     # 1-材料纯图片
     material_type = models.IntegerField(default='1')
     # 材料列表，半角逗号隔开
@@ -59,6 +59,7 @@ class Score(models.Model):
     answer = models.CharField(max_length=128, default='', blank=True)
     score = models.IntegerField()
     elapsed_seconds = models.IntegerField(default=0, blank=True)
+    submitted = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
